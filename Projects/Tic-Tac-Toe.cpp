d2d23f17c2c823a0aba971ed423286e2321ab79e
Poison-Fang <ayushwalia999@gmail.com>
2018-09-15 13:11:47 +0530

    Tic-Tac-Toe:Improve PC's Playing Algorithm! * Hopefully it's unbeatable now! Fingers Crossed.

--- Projects/Tic-Tac-Toe.cpp
+++ Projects/Tic-Tac-Toe.cpp
@@ -2,7 +2,9 @@
 #include <cstdlib>
 using namespace std;
 
+int input;
 char arr[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
 void clear(){
     #ifdef _WIN32
     system("cls");
@@ -10,6 +12,7 @@ void clear(){
     system("clear");
     #endif
 }
+
 int checker(int &i){
   if((arr[1] == arr[2]) && (arr[2] == arr[3]))
     return 1;
@@ -32,6 +35,7 @@ int checker(int &i){
     exit(0);
   }
 }
+
 void printGrid(int& i){
   clear();
   cout<<"Player 1: X   vs   Player 2: O"<<endl<<endl;
@@ -46,27 +50,8 @@ void printGrid(int& i){
   cout<<"     |     |     "<<endl<<endl;
   i++;
 }
-int bestMove(){
-    if(arr[1]!='X' && arr[1]!='O')
-        return 1;
-    else if(arr[3]!='X' && arr[3]!='O')
-        return 1;
-    else if(arr[7]!='X' && arr[7]!='O')
-        return 1;
-    else if(arr[9]!='X' && arr[9]!='O')
-        return 1;
-    else if(arr[2]!='X' && arr[2]!='O')
-        return 2;
-    else if(arr[4]!='X' && arr[4]!='O')
-        return 2;
-    else if(arr[6]!='X' && arr[6]!='O')
-        return 2;
-    else if(arr[8]!='X' && arr[8]!='O')
-        return 2;
-    else
-        return 0;
-}
-int bestMove(int& i){
+
+int bestMove(int& i,int input){
     int tmp=0;
     if(i>4)
     for(int j=1;j<=9;j++)
@@ -80,7 +65,7 @@ int bestMove(int& i){
             else
                 arr[j]='0'+j;
         }
-    if(!tmp)
+    if(!tmp&&i>2)
     for(int j=1;j<=9;j++)
         if(arr[j]!='X' && arr[j]!='O'){
             arr[j]='X';
@@ -93,11 +78,59 @@ int bestMove(int& i){
             else
                  arr[j]='0'+j;
         }
+    if(!tmp){
+        if(input==5 || arr[5]=='X'){
+            int a[4]={1,3,7,9};
+            while(1){
+            input=a[rand()%4];
+            if(arr[input]!='X'&&arr[input]!='O'){
+                arr[input]='O';
+                tmp=1;
+                return 1;
+                break;
+                }
+             else if((a[1]=='X'||a[1]=='O')&&(a[3]=='X'||a[3]=='O')&&(a[7]=='X'||a[7]=='O')&&(a[9]=='X'||a[9]=='O'))
+                    tmp=1;
+                    break;
+            }
+        }
+        else{
+        int a[2];
+        if(input==1)
+            a[0]=2,a[1]=4;
+        else if(input==2)
+            a[0]=1,a[1]=3;
+        else if(input==3)
+            a[0]=2,a[1]=6;
+        else if(input==4)
+            a[0]=1,a[1]=7;
+        else if(input==6)
+            a[0]=3,a[1]=9;
+        else if(input==7)
+            a[0]=4,a[1]=8;
+        else if(input==8)
+            a[0]=7,a[1]=9;
+        else if(input==9)
+            a[0]=8,a[1]=6;
+        while(1){
+        input=a[rand()%2];
+        if(arr[input]!='X'&&arr[input]!='O'){
+            arr[input]='O';
+            tmp=1;
+            return 1;
+            break;
+            }
+         else if((a[0]=='X' || a[0]=='O') && (a[1]=='X' || a[1]=='O'))
+                tmp=1;
+                break;
+        }
+      }
+    }
     if(!tmp)
         return 0;
 }
+
 void gameManager(int& i,int mode){
-    int input;
     printGrid(i);
     if(i>2){
         if(checker(i)==1){
@@ -140,17 +173,9 @@ void gameManager(int& i,int mode){
         arr[5]='O';
         break;
         }
-        if(i>2&&bestMove(i)==1){
+        if(bestMove(i,input)==1){
             break;
         }
-        if(arr[5]=='O'&&bestMove()==2){
-            int a[4]={2,4,6,8};
-            input=a[rand()%4];
-        }
-        else if(arr[5]=='X'&&bestMove()==1){
-            int a[4]={1,3,7,9};
-            input=a[rand()%4];
-          }
         else
         input=rand()%9+1;
         if(arr[input]!='X'&&arr[input]!='O'){
@@ -162,6 +187,7 @@ void gameManager(int& i,int mode){
     }
     gameManager(i,mode);
 }
+
 int main()
 {
     int choice,mode,i=-1;
